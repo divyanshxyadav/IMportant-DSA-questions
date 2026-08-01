7967fa52b1868dba98f422dac4ecebeb6305eef7
Divyansh <divyanshyadavforyou@gmail.com>
2026-08-01 09:31:38 +0530

    Time: 0 ms (100%), Space: 8.3 MB (99.63%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l=1,r=x;
+        int ans=0;
+        while(l<=r){
+            int m=l+(r-l)/2;
+if(m<=x/m){
+    ans=max(ans,m);
+     l=m+1;
+}
+else{r=m-1;
+   
+}
+ 
+        }
+  return ans;
+    }
+};
