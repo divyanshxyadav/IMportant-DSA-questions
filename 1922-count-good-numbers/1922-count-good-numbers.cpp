0613e6916836c5ef715ae3af7d8a946553f1dd34
Divyansh <divyanshyadavforyou@gmail.com>
2026-09-10 15:46:20 +0530

    Time: 0 ms (100%), Space: 8 MB (37.92%) - LeetHub

---
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+const int M=1e9+7;
+int fpow(long long a,long long b){
+    if(b==0)
+    return 1;
+    long long half=fpow(a,b/2);
+    long long res=(half*half)%M;
+    if(b%2==1){
+        res=(res*a)%M;
+    }
+    return res;
+}
+    int countGoodNumbers(long long n) {
+        return (long long)fpow(5,(n+1)/2)*fpow(4,n/2)%M;
+    }
+};
